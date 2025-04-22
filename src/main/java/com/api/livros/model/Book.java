a356375b1cb82ac1ca3cdfded6c5ea2fce5766f2
Giovani e Jesuino <giovani.eugenio.sp@gmail.com>
2025-04-22 11:31:04 -0300

    definido a entidade Book

---
+++ src/main/java/com/api/livros/model/Book.java
@@ -0,0 +1,24 @@
+package com.api.livros.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter @Setter
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String author;
+    private String isbn;
+    private Integer publicationYear;
+}
