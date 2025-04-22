3e0397b956ff79c9c1612fe57a446a49daa4323e
Giovani e Jesuino <giovani.eugenio.sp@gmail.com>
2025-04-22 11:33:31 -0300

    criado o repositorio para Book e definindo o metodo findByAuthor

---
+++ src/main/java/com/api/livros/repository/BookRepository.java
@@ -0,0 +1,13 @@
+package com.api.livros.repository;
+
+import com.api.livros.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+    List<Book> findByAuthor(String author);
+}
